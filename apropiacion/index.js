be76cc5b291dd395e1dd04cadd1e84b37ee4a587
Jhon-Gelvez <jhonjairogelvezgomez@gmail.com>
2026-04-27 20:26:08 -0500

    Replace exports from ejercicio5.js with procesosConPromesas

--- apropiacion/index.js
+++ apropiacion/index.js
@@ -2,8 +2,7 @@ export { asincroniaBasica } from "./ejercicio1.js";
 export { codigoBloqueante } from "./ejercicio2.js";
 export { procesarPedido } from "./ejercicio3.js";
 export { procesosEncadenados  } from "./ejercicio4.js";
-export { tomarDatos  } from "./ejercicio5.js";
-export {  procesar } from "./ejercicio5.js";
+export { procesosConPromesas } from "./ejercicio5.js";
 
 
 export { consultaUsuarioCallback, consultaUsuarioPromesas, consultaUsuarioAsync } from "./ejercicio8_pt1.js";
